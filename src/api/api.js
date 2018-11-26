bf422003e98cfb931c3611a6611fa2381817ff52
Eduardo Reveles <me@osiux.ws>
2018-11-26 00:27:13 -0600

    wretch as resource

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,4 @@
+import wretch from "wretch";
 import { unstable_createResource } from "react-cache";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
@@ -7,20 +8,35 @@ const headers = {
     "Content-Type": "application/json",
 };
 
-const apiResource = unstable_createResource(async path => {
-    const url = `${BASE_URL}?api_key=${API_KEY}&format=json&${path}`;
-    const response = await fetch(url, { headers });
+const apiResource = unstable_createResource(
+    async (query = {}) => {
+        const api = wretch()
+            .url(BASE_URL)
+            .query({
+                api_key: API_KEY,
+                format: "json",
+                ...query,
+            })
+            .headers(headers);
 
-    return await response.json();
-});
+        return await api.get().json();
+    },
+    value => {
+        return JSON.stringify(value);
+    },
+);
 
 const getTopArtists = () =>
-    apiResource.read("method=chart.gettopartists&limit=16");
+    apiResource.read({ method: "chart.gettopartists", limit: 16 });
 
 const getArtistInfo = artistId =>
-    apiResource.read(`method=artist.getinfo&mbid=${artistId}`);
+    apiResource.read({ method: "artist.getinfo", mbid: artistId });
 
 const getArtistAlbums = artistId =>
-    apiResource.read(`method=artist.gettopalbums&mbid=${artistId}&limit=16`);
+    apiResource.read({
+        method: "artist.gettopalbums",
+        mbid: artistId,
+        limit: 16,
+    });
 
 export { getTopArtists, getArtistInfo, getArtistAlbums };
